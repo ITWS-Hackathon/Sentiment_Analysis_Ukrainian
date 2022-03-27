9d321fab7203d91780c8265e6b8e0de1b4190255
mrxy56 <mrxy56@gmail.com>
2022-03-27 15:06:05 -0400

    update

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,52 +1,65 @@
 import numpy as np
-import pandas as pd
-import nltk
-
-import os
-
-df_review = [line.rstrip() for line in open('tweets.csv')]
-print (len(df_review))
 import pandas
-df_review = pandas.read_csv('tweets.csv', sep='\t')
-df_review.head()
-df_review.describe()
-df_review.groupby('rating').describe()
-df_review['length'] = df_review['verified_reviews'].apply(len)
-df_review.head()
 import matplotlib.pyplot as plt
-import seaborn as sns
-df_review['length'].plot(bins=50, kind='hist')
-df_review.length.describe()
-df_review[df_review['length'] == 2851]['verified_reviews'].iloc[0]
-df_review.hist(column='length', by='feedback', bins=50,figsize=(10,4))
-import numpy as np
-import matplotlib.pyplot as plt
-dataset = pd.read_csv('../tweets.csv', delimiter = '\t', quoting = 3)
 import re
 import nltk
+from textblob import TextBlob
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from sklearn.model_selection import StratifiedShuffleSplit
+
+def polarity(tweet):
+    analysis = TextBlob(tweet)
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity < 0:
+        return -1
+    else:
+        return 0
+
+X_data = pandas.read_csv(open('tweets.csv'), delimiter=",")
+df_tweet = X_data['Tweets']
+N = len(df_tweet)
 corpus=[]
-for i in range(0,3150):
-    review = re.sub('[^a-zA-Z]', ' ', dataset['verified_reviews'][i] )
-    review=review.lower()
-    review=review.split()
-    ps=PorterStemmer()
-    review=[ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-    review=' '.join(review)
-    corpus.append(review)
+y_feature = []
+#nltk.download('stopwords')
+# Data Cleaning
+pos = 0
+neg = 0
+neu = 0
+for i in range(N):
+    tweet = re.sub('[^a-zA-Z]', ' ', df_tweet[i]) # Remove anything that is not alphabets
+    tweet = tweet.lower()
+    tweet = tweet.split()
+    ps = PorterStemmer()
+    tweet = [ps.stem(word) for word in tweet if not word in set(stopwords.words('english'))]
+    tweet = ' '.join(tweet)
+    corpus.append(tweet)
+    p = polarity(tweet)
+    if p == 1:
+        pos += 1
+        y_feature.append(1)
+    elif p == -1:
+        neg += 1
+        y_feature.append(-1)
+    else:
+        neu += 1
+        y_feature.append(0)
 
 from sklearn.feature_extraction.text import CountVectorizer
-cv=CountVectorizer(max_features=1500)
-X=cv.fit_transform(corpus).toarray()
-y=dataset.iloc[:,4].values
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
 
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=0)
-nX = None
-ny = None
+X = np.asarray(corpus)
+X = X.reshape(N, -1)
+y_feature = np.asarray(y_feature)
+# Split
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
 for train_index, test_index in sss.split(X, y_feature):
-        nX = X[test_index].copy()
-        ny = y_feature[test_index].copy()
+    X_train = X[train_index].copy()
+    y_train = y_feature[train_index].copy()
+    X_test = X[test_index].copy()
+    y_test = y_feature[test_index].copy()
 
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy', random_state = 0)
@@ -55,5 +68,5 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-cm
+matrix = confusion_matrix(y_test, y_pred)
+print(matrix)
